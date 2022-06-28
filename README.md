f81c7200041c4bfad2e91129c9449983b7203f2f
Akashdeep Nandi <adncorwin@gmail.com>
2022-06-28 15:11:28 +0530

    feat(data, index, app): new data, category wise searching and styling updates

--- README.md
+++ README.md
@@ -5,6 +5,34 @@
   - [x] Add decent styling
   - [x] Make table responsive
 - [x] Render searching results
+- [ ] Add TOC
+- [ ] Add more information section
+  - [ ] twitter handles
+  - [ ] websites
+- [ ] Create a form for intake of more helplines
 - [ ] Sorting of data
-- [ ] Add more data from official sources
+- [ ] Provide a download as csv button
 - [ ] If numbers grow more, consider adding a backend
+
+
+```bash
+# curl to get the time info
+res = await fetch("https://ffs.india-water.gov.in/iam/api/layer-station/010-MBDGHY", {
+    "credentials": "omit",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/json",
+        "class-name": "ForecastDetailLayerStationDto",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache"
+    },
+    "referrer": "https://ffs.india-water.gov.in/main/station-detail/010-MBDGHY",
+    "method": "GET",
+    "mode": "no-cors"
+});
+
+# curl to see current water level
+curl 'https://ffs.india-water.gov.in/iam/api/new-forecasted-entry-data/specification/sorted?sort-criteria=%7B%22sortOrderDtos%22:%5B%7B%22sortDirection%22:%22ASC%22,%22field%22:%22id.forecastedDate%22%7D%5D%7D&specification=%7B%22where%22:%7B%22where%22:%7B%22where%22:%7B%22where%22:%7B%22expression%22:%7B%22valueIsRelationField%22:false,%22fieldName%22:%22id.stationCode%22,%22operator%22:%22eq%22,%22value%22:%22010-MBDGHY%22%7D%7D,%22and%22:%7B%22expression%22:%7B%22valueIsRelationField%22:false,%22fieldName%22:%22id.datatypeCode%22,%22operator%22:%22eq%22,%22value%22:%22HHS%22%7D%7D%7D,%22and%22:%7B%22expression%22:%7B%22valueIsRelationField%22:false,%22fieldName%22:%22id.forecastedDate%22,%22operator%22:%22gt%22,%22value%22:%222022-06-26T17:15:17.907%22%7D%7D%7D,%22and%22:%7B%22expression%22:%7B%22valueIsRelationField%22:false,%22fieldName%22:%22realValue%22,%22operator%22:%22null%22,%22value%22:%22false%22%7D%7D%7D,%22and%22:%7B%22expression%22:%7B%22valueIsRelationField%22:true,%22fieldName%22:%22savedAt%22,%22operator%22:%22eq%22,%22value%22:%22forecastNoAggregate.maxDate%22%7D%7D%7D' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'class-name: NewForecastedEntryDataDto' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://ffs.india-water.gov.in/main/station-detail/010-MBDGHY' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache'
+```
--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const searchInput = document.querySelector("#search");
 const tbody = document.querySelector("tbody");
+const subContents = document.querySelector(".sub-contents");
+console.log(subContents);
+
+const createCategoryLink = (category) => {
+  const categoryTitle = categories[category];
+  const html = `
+  <li><a href="#${category}">${categoryTitle} &#8595;</a></li>
+  `
+  return html;
+}
+
+const renderCategoryLinks = (catKeys) => {
+  const html = catKeys.reduce((acc, cat) => {
+    const link = createCategoryLink(cat);
+    acc += `${link}`;
+    return acc;
+  }, "");
+
+  subContents.innerHTML = html;
+
+}
 
 const createTableRow = ({ name, description, address, numbers }) => {
   const numbers_string = numbers.reduce((acc, num) => {
@@ -19,29 +40,55 @@ const createTableRow = ({ name, description, address, numbers }) => {
   const descString = description.trim() ? 
   `<div class="description">${description}</div>\n` : "";
   const nameString = name.trim() ? 
-  `<div class="name">Name: ${name}</div>\n` : "";
+  `<div class="name"><b>Name</b>: ${name}</div>\n` : "";
   const addrString = address.trim() ? 
-  `<div class="address">Address: ${address}</div>\n` : "";
+  `<div class="address"><b>Address</b>: ${address}</div>\n` : "";
 
   const row = `
-    <tr>
-      <td class="details">
-        ${descString}
-        ${nameString}
-        ${addrString}
-      </td>
-      <td class="numbers">${numbers_string}</td>
-    </tr>
+  <tr>
+    <td class="details">
+      ${descString}
+      ${nameString}
+      ${addrString}
+    </td>
+    <td class="numbers">${numbers_string}</td>
+  </tr>
   `
   return row;
 }
 
-const renderTable = (data) => {
-    const html = data.reduce((acc, rowData) => {
-      acc += createTableRow(rowData);
-      return acc;
-    }, "");
-    tbody.innerHTML = html;
+const createCategoryRow = (category, data) => {
+  const categoryTitle = categories[category];
+  const html = data.reduce((acc, rowData) => {
+    acc += createTableRow(rowData);
+    return acc;
+  }, "");
+
+  const cRow = `
+  <tr class="category" id="${category}">
+    <td colspan="2">${categoryTitle}</td>
+  </tr>
+  ${html}
+  `
+  return cRow;
+}
+
+const getCategoryRowHTML = (catData) => {
+  const catKeys = Object.keys(catData);
+  const categoryRowHTML = catKeys.reduce((acc, cat) => {
+    const data = catData[cat];
+    if(data.length == 0) return acc;
+    let catHTML = createCategoryRow(cat, data);
+    acc+= `${catHTML}\n`;
+    return acc;
+  }, "");
+
+  return categoryRowHTML;
+}
+
+const renderTable = (catData) => {
+    const categoryRowHTML = getCategoryRowHTML(catData);
+    tbody.innerHTML = categoryRowHTML;
 }
 
 
@@ -49,10 +96,18 @@ const renderTable = (data) => {
 const handleSearch = (e) => {
   const search = e.target.value;
   if(!search) {
-    renderTable(helplineData);
+    renderTable(categoryData);
     return;
   }
-  const filteredData = helplineData.filter(({name, description, address, numbers}) => {
+
+  const catKeys = Object.keys(categories);
+  let filteredCatData = {};
+  const filteredCatKeys = catKeys.filter((cat) => {
+  const title = categories[cat]; 
+  const inTitle = title.toLowerCase().includes(search.toLowerCase());
+  const data = categoryData[cat];
+  const filteredData = data
+  	.filter(({name, description, address, numbers}) => {
     const InName = name.toLowerCase().includes(search.toLowerCase());
     const InDesc = description.toLowerCase().includes(search.toLowerCase());
     const InAddress = address.toLowerCase().includes(search.toLowerCase());
@@ -60,11 +115,16 @@ const handleSearch = (e) => {
       const numString = `${num}`;
       return numString.includes(search)
     }).length > 0;
+      
+      return InName || InDesc || InAddress || InNums;
+    }); 
 
-    return InName || InDesc || InAddress || InNums;
-  });
+    const inData = filteredData.length > 0;
+    filteredCatData[cat] = !inData && inTitle ? data : filteredData;
+    return inTitle || inData;
+  }); 
 
-  renderTable(filteredData);
+  renderTable(filteredCatData);
 }
 
 function debounce(func, timeout = 300){
@@ -91,4 +151,6 @@ const copyToClipboard = async (num) => {
 
 searchInput.addEventListener("keyup", debouncedSearch);
 searchInput.value = "";
-renderTable(helplineData);
+renderTable(categoryData);
+renderCategoryLinks(Object.keys(categories));
+
